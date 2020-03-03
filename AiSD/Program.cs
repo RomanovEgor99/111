31d78cff9eef2ac106945dadb41880ed7f942af2
RomanovEgor99 <romanov.egor2010.ru@gmail.com>
2020-03-03 21:39:03 +0400

    1

--- AiSD/Program.cs
+++ AiSD/Program.cs
@@ -10,6 +10,47 @@ namespace AiSD
     {
         static void Main(string[] args)
         {
+            uint[] arrayOfUint = new uint[10];
+            Console.WriteLine("Одномерный массив беззнаковых целых чисел: ");
+            foreach (uint i in arrayOfUint)
+            {
+                Console.Write(" {0}", i);
+            }
+            Console.WriteLine("\nСтатический одномерный массив символов:");
+            char[] arrayOfChar = new char[10];
+            foreach (char j in arrayOfUint)
+            {
+                Console.Write(" {0}", j);
+            }
+            int[,,] arrayOfInt = new int[10, 10, 3];
+            Console.WriteLine("\nТрехмерный массив целых чисел:");
+
+            for (int k = 0; k < 3; k++)
+            {
+                Console.Write("Массив №{0} \n", k);
+                for (int j = 0; j < 10; j++)
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        Console.Write("{0}, ", arrayOfInt[i, j, k]);
+
+                    }
+                    Console.WriteLine();
+
+                }
+                Console.WriteLine("\n");
+            }
+            Random rand = new Random();
+            Console.WriteLine("Одномерный массив беззнаковых целых чисел: ");
+            foreach (uint i in arrayOfUint)
+            {
+                arrayOfUint[i] = (uint)rand.Next(1, 99);
+            }
+            foreach (uint i in arrayOfUint)
+            {
+                Console.Write(" {0}", i);
+            }
+            Console.ReadLine();
         }
     }
 }
